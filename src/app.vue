277e71824db9e1782ee604db5f721bc74c625664
Dsoft Developer <namtt@d-soft.com.vn>
2024-11-28 15:58:32 +0700

    Feature: Dark mode by default

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-   <div class="dark bg-background text-foreground h-screen font-geist">
+   <div class="h-screen font-geist">
       <NuxtLayout name="primary-default">
          <NuxtPage />
       </NuxtLayout>
